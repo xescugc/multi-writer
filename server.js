ceb009d3ae4bb7dd5f26117ac7c1c642f8b30f2b
xescuGC <xescugil@gmail.com>
2014-06-17 03:14:29 +0200

    FIRST COMMIT

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app     = express();
+var server  = require('http').Server(app);
+var io      = require('socket.io').listen(server);
+
+var port = Number(process.env.PORT || 5000);
+server.listen(port, function() {
+    console.log("Listening on " + port);
+});
+
+app.use('/public/components', express.static(__dirname + '/public/components'));
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+  socket.join('multi-writer');
+  socket.on('keypress', function (data) {
+    io.sockets.in('multi-writer').emit('keypress', data);
+  });
+});
